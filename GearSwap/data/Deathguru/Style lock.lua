100df4d43847141f531b778dd5f59791e7b24137
d0peguru <153754820+d0peguru@users.noreply.github.com>
2024-03-02 13:48:26 -0600

    Added  addon folders

---
+++ GearSwap/data/Deathguru/Style lock.lua
@@ -0,0 +1,45 @@
+function user_job_lockstyle()
+	if player.sub_job == 'NIN' or player.sub_job == 'DNC' then
+		if player.equipment.main == nil or player.equipment.main == 'empty' then
+			windower.chat.input('/lockstyleset 021')
+		elseif res.items[item_name_to_id(player.equipment.main)].skill == 3 then --Sword in main hand.
+			if res.items[item_name_to_id(player.equipment.sub)].skill == 3 then --Sword/Sword.
+				windower.chat.input('/lockstyleset 021')
+			elseif res.items[item_name_to_id(player.equipment.sub)].skill == 2 then --Sword/Dagger.
+				windower.chat.input('/lockstyleset 022')
+			elseif res.items[item_name_to_id(player.equipment.sub)].skill == 11 then --Sword/Club.
+				windower.chat.input('/lockstyleset 022')
+				elseif res.items[item_name_to_id(player.equipment.sub)].shield_size then --Sword/Shield.
+				windower.chat.input('/lockstyleset 024')
+			else
+				windower.chat.input('/lockstyleset 021') --Catchall
+			end
+		elseif res.items[item_name_to_id(player.equipment.main)].skill == 2 then --Dagger in main hand.
+			if res.items[item_name_to_id(player.equipment.sub)].skill == 3 then --Dagger/Sword.
+				windower.chat.input('/lockstyleset 021')
+			elseif res.items[item_name_to_id(player.equipment.sub)].skill == 2 then --Dagger/Dagger.
+				windower.chat.input('/lockstyleset 021')
+			elseif res.items[item_name_to_id(player.equipment.sub)].skill == 11 then --Dagger/Club.
+				windower.chat.input('/lockstyleset 022')
+			else
+				windower.chat.input('/lockstyleset 021') --Catchall
+			end
+		elseif res.items[item_name_to_id(player.equipment.main)].skill == 11 then --Club in main hand.
+			if res.items[item_name_to_id(player.equipment.sub)].skill == 3 then --Club/Sword.
+				windower.chat.input('/lockstyleset 021')
+			elseif res.items[item_name_to_id(player.equipment.sub)].skill == 2 then --Club/Dagger.
+				windower.chat.input('/lockstyleset 021')
+			elseif res.items[item_name_to_id(player.equipment.sub)].skill == 11 then --Club/Club.
+				windower.chat.input('/lockstyleset 022')
+			else
+				windower.chat.input('/lockstyleset 024') --Catchall
+			end
+		end
+	elseif player.sub_job == 'WHM' or state.Buff['Light Arts'] or state.Buff['Addendum: White'] then
+		windower.chat.input('/lockstyleset 024')
+	elseif player.sub_job == 'BLM' or state.Buff['Dark Arts'] or state.Buff['Addendum: Black'] then
+		windower.chat.input('/lockstyleset 024')
+	else
+		windower.chat.input('/lockstyleset 024')
+	end
+end
